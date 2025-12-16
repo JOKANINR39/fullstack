2fa4de62c9297d200d42271d1031a148f6c45538
JOKANINR39 <23BCS039@PSGRKCW.AC.IN>
2025-12-16 14:24:02 +0530

    Add files via upload

---
+++ Program/EventHandling/app.jsx
@@ -0,0 +1,13 @@
+#app.jsx
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import EventHandlingExample from './core concepts/Helloworld/EventHandling'
+
+export default function App(){
+  return<EventHandlingExample/>
+}
+
+
